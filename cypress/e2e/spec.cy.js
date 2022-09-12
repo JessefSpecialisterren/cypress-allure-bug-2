e27c4d3be28577c2ab641b0bff7d3e077abbee25
Jesse Feddema <jessef@specialisterren.nl>
2022-09-12 14:00:42 +0200

    Update dependencies and tweak the spec somewhat

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress" />
-describe('cypress-allure-plugin marks tests as failed that didn\'t fail', () => {
+describe('cypress-allure-plugin marks a failed test as Skipped', () => {
   let counter = 0
 
   beforeEach(() => {
     counter++
-    if (counter === 2 || counter >= 6) throw new Error('Fail test B once and test E always')
+    if (counter === 2 || counter >= 5) throw new Error('Fail test B once and test D always')
   })
 
   it('A', () => {})
